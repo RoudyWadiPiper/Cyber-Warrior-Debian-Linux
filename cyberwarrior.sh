8c8b26fd13209eecf332ebfbc12966da310ddc69
RoudyWadiPiper <levi@aolson.net>
2024-10-13 00:35:33 -0500

    Add files via upload

---
+++ cyberwarrior.sh
@@ -0,0 +1,7 @@
+sudo cp /etc/login.defs /
+mv /login.defs /BACKUPlogin.defs
+sed -i 's/^PASS_MAX_DAYS.*/PASS_MAX_DAYS   90/g' /etc/login.defs
+sed -i 's/^PASS_MIN_DAYS.*/PASS_MIN_DAYS   3/g' /etc/login.defs
+sed -i 's/^PASS_WARN_AGE.*/PASS_WARN_AGE   7/g' /etc/login.defs
+sed -i 's/^LOGIN_RETRIES.*/LOGIN_RETRIES   5/g' /etc/login.defs
+sed -i 's/^LOGIN_TIMEOUT.*/LOGIN_TIMEOUT   60/g' /etc/login.defs
