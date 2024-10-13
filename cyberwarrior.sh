83e6fd724b267d8b18e992ed673387326097af40
RoudyWadiPiper <levi@aolson.net>
2024-10-13 09:34:51 -0500

    Update cyberwarrior.sh

--- cyberwarrior.sh
+++ cyberwarrior.sh
@@ -5,3 +5,4 @@ sed -i 's/^PASS_MIN_DAYS.*/PASS_MIN_DAYS   3/g' /etc/login.defs
 sed -i 's/^PASS_WARN_AGE.*/PASS_WARN_AGE   7/g' /etc/login.defs
 sed -i 's/^LOGIN_RETRIES.*/LOGIN_RETRIES   5/g' /etc/login.defs
 sed -i 's/^LOGIN_TIMEOUT.*/LOGIN_TIMEOUT   60/g' /etc/login.defs
+echo CyberWarrior Has Completed!
